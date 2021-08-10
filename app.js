27dd2f89b00658147fc086a55ab01fa5861852a5
Omid BakhshiPour <bakhshipouromid@gmail.com>
2021-08-11 04:21:11 +0430

    create delete campground button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -22,7 +22,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -56,11 +56,20 @@ app.get("/campgrounds/:id/edit", async (req, res) => {
     res.render("campgrounds/edit", { campground });
 });
 
-app.put('/campgrounds/:id', async(req, res) => {
+app.put("/campgrounds/:id", async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, req.body.campground);
+    const campground = await Campground.findByIdAndUpdate(
+        id,
+        req.body.campground
+    );
     res.redirect(`/campgrounds/${campground._id}`);
-})
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+});
 
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000");
