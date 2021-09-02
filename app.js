743bb3e400e8d8444a1375b7e6b25054a7872408
Omid BakhshiPour <bakhshipouromid@gmail.com>
2021-09-02 16:12:21 +0430

    Create reviews router

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const { campgroundSchema } = require("./schemas.js");
 const catchAsync = require("./utils/catchAsync");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
-const ExpressError = require("./utils/ExpressError"); 
+const ExpressError = require("./utils/ExpressError");
+const { required } = require("joi");
+const Review = require('./models/review');
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
@@ -106,6 +108,15 @@ app.delete(
         res.redirect("/campgrounds");
     })
 );
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save(); 
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
 
 app.all("*", (req, res, next) => {
     next(new ExpressError("Page Not Found!", 404));
