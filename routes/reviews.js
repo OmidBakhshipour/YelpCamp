fc400a8b5a36ca4926292c1b4c9e65aa6dfffab3
Omid BakhshiPour <bakhshipouromid@gmail.com>
2021-09-09 19:13:12 +0430

    Restructure routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 const catchAsync = require("../utils/catchAsync");
-const Campground = require("../models/campground");
-const Review = require("../models/review");
 const reviews = require("../controllers/reviews");
 
 router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
