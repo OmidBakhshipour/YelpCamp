2686783b5c211c9eaf5f8a5576695ac9af4da863
Omid BakhshiPour <bakhshipouromid@gmail.com>
2021-09-09 19:00:23 +0430

    create campground, review and user controller to refactor codes

---
+++ controllers/campgrounds.js
@@ -0,0 +1,74 @@
+const Campground = require("../models/campground");
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+};
+
+module.exports.renderNewForm = (req, res) => {
+    res.render("campgrounds/new");
+};
+
+module.exports.createCampground = async (req, res, next) => {
+    // req.body : {"campground":{"title":"something","location":"something", ...}}
+    // if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+    const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
+    await campground.save();
+    req.flash("success", "Succesfully made a new campground");
+    res.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.showCampground = async (req, res) => {
+    const { id } = req.params;
+    /*
+    const campground = await Campground.findById(id).populate("reviews").populate("author");
+    Here the author is the campground's author, we want the author of each review.
+    What should we do? Nested populate. down below
+    */
+    const campground = await Campground.findById(id)
+        .populate({
+            path: "reviews",
+            populate: {
+                path: "author",
+            },
+        })
+        .populate("author");
+    // Telling first populate the reviews and their authors, then populate the campground's author.
+    if (!campground) {
+        req.flash("error", "No Campground Found");
+        return res.redirect("/campgrounds");
+    }
+    res.render("campgrounds/show", { campground });
+};
+
+module.exports.renderEditForm = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground) {
+        req.flash("error", "No Campground Found");
+        return res.redirect("/campgrounds");
+    }
+    res.render("campgrounds/edit", { campground });
+};
+
+module.exports.updateCampground = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(
+        id,
+        req.body.campground
+    );
+    req.flash("success", "Succesfully updated campground");
+    res.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.deleteCampground = async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    req.flash("success", "Successfully deleted campground");
+    res.redirect("/campgrounds");
+};
+
+
+
+
--- routes/reviews.js
+++ routes/reviews.js
@@ -4,35 +4,15 @@ const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
+const reviews = require("../controllers/reviews");
 
-router.post(
-    "/",
-    isLoggedIn,
-    validateReview,
-    catchAsync(async (req, res) => {
-        const campground = await Campground.findById(req.params.id);
-        const review = new Review(req.body.review);
-        review.author = req.user._id;
-        campground.reviews.push(review);
-        await review.save();
-        await campground.save();
-        req.flash("success", "Created new review!");
-        res.redirect(`/campgrounds/${campground._id}`);
-    })
-);
+router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 router.delete(
     "/:reviewId",
     isLoggedIn,
     isReviewAuthor,
-    catchAsync(async (req, res) => {
-        const { id, reviewId } = req.params;
-        Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-        // It's gonna take reviewId and pull anything with that id out of reviews.
-        await Review.findByIdAndDelete(reviewId);
-        req.flash("success", "Successfully deleted review");
-        res.redirect(`/campgrounds/${id}`);
-    })
+    catchAsync(reviews.deleteReview)
 );
 
 module.exports = router;
