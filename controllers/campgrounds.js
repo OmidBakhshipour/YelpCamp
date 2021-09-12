0bf2d403198aeb8ba986435fbfac9d5f8c05904b
Omid BakhshiPour <bakhshipouromid@gmail.com>
2021-09-12 23:44:00 +0430

    add upload to edit page

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -13,10 +13,10 @@ module.exports.createCampground = async (req, res, next) => {
     // req.body : {"campground":{"title":"something","location":"something", ...}}
     // if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
     const campground = new Campground(req.body.campground);
-    campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename }));  // req.files is from multer
+    campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename })); // req.files is from multer
     campground.author = req.user._id;
     await campground.save();
-    console.log(campground)
+    console.log(campground);
     req.flash("success", "Succesfully made a new campground");
     res.redirect(`/campgrounds/${campground._id}`);
 };
@@ -56,10 +56,10 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(
-        id,
-        req.body.campground
-    );
+    const campground = await Campground.findByIdAndUpdate(id, req.body.campground);
+    const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs); // req.files is from multer
+    await campground.save();
     req.flash("success", "Succesfully updated campground");
     res.redirect(`/campgrounds/${campground._id}`);
 };
