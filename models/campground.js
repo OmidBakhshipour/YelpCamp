8a6bade688f8c1f6a2f6bf3f100d427d1fd5251e
Omid BakhshiPour <bakhshipouromid@gmail.com>
2021-09-13 21:41:26 +0430

    add a thumbnail virtual property

--- models/campground.js
+++ models/campground.js
@@ -3,12 +3,18 @@ const Review = require("./review");
 // Shorthand for Schema
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroudSchema = new Schema({
     title: String,
-    images: [{
-        url: String,
-        filename: String
-    }],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
