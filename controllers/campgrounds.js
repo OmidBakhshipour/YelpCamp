e3efb04166883d3fa9fbdc7a24d9362c40e24fdb
Omid BakhshiPour <bakhshipouromid@gmail.com>
2021-09-15 22:07:05 +0430

    Geocoding our locations

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,7 @@
 const Campground = require("../models/campground");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
@@ -13,13 +16,18 @@ module.exports.renderNewForm = (req, res) => {
 module.exports.createCampground = async (req, res, next) => {
     // req.body : {"campground":{"title":"something","location":"something", ...}}
     // if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
-    const campground = new Campground(req.body.campground);
-    campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename })); // req.files is from multer
-    campground.author = req.user._id;
-    await campground.save();
-    console.log(campground);
-    req.flash("success", "Succesfully made a new campground");
-    res.redirect(`/campgrounds/${campground._id}`);
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 2
+    }).send();
+    res.send(geoData.body.features[0].geometry.coordinates);
+    // const campground = new Campground(req.body.campground);
+    // campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename })); // req.files is from multer
+    // campground.author = req.user._id;
+    // await campground.save();
+    // console.log(campground);
+    // req.flash("success", "Succesfully made a new campground");
+    // res.redirect(`/campgrounds/${campground._id}`);
 };
 
 module.exports.showCampground = async (req, res) => {
@@ -62,7 +70,7 @@ module.exports.updateCampground = async (req, res) => {
     campground.images.push(...imgs); // req.files is from multer
     await campground.save();
     if (req.body.deleteImages) {
-        for(let filename of req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
             await cloudinary.uploader.destroy(filename);
         }
         await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
