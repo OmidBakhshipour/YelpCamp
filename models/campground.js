8101002a56f2c4d44e1a090a751dd74ee96e4f9f
Omid BakhshiPour <bakhshipouromid@gmail.com>
2021-09-16 12:14:02 +0430

    update campground model to contain geometry location data

--- models/campground.js
+++ models/campground.js
@@ -5,16 +5,27 @@ const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
     url: String,
-    filename: String
-})
+    filename: String,
+});
 
-ImageSchema.virtual('thumbnail').get(function() {
-    return this.url.replace('/upload', '/upload/w_200');
+ImageSchema.virtual("thumbnail").get(function () {
+    return this.url.replace("/upload", "/upload/w_200");
 });
 
 const CampgroudSchema = new Schema({
     title: String,
     images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ["Point"], // 'location.type' must be 'Point'
+            required: true,
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+        },
+    },
     price: Number,
     description: String,
     location: String,
